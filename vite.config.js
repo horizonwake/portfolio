edbd3896eee257346ba3f6b104c64c1276a8e47d
horizonwake <jarredbrownmusic@gmail.com>
2026-02-02 12:17:46 -0500

    added CNAME and favicon to public folder

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({ command }) => ({
-  base: command === "/", // Set base path for production
+export default defineConfig({
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-}));
+});
