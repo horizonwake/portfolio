341850891a205d78cc381afb42bbdbd51da09cfb
horizonwake <jarredbrownmusic@gmail.com>
2026-02-02 11:09:57 -0500

    conditionally set base path to load assets in prod

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "/" : "/portfolio/", // Set base path for production
   plugins: [vue()],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-});
+}));
